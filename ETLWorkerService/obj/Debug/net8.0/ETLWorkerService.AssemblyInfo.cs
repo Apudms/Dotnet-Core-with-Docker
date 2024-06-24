8464f48012124a1b95b31b34d47aeb02b16267f3
Muhammad Saefudin <Muhammad.Saefudin@rapidtech.id>
2024-06-24 23:45:58 +0700

    Add FileSettings Class

--- ETLWorkerService/obj/Debug/net8.0/ETLWorkerService.AssemblyInfo.cs
+++ ETLWorkerService/obj/Debug/net8.0/ETLWorkerService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETLWorkerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603dbf48f54197100b7df6ea98f227d9e8df2268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44bf32142529bfd1f9bca418363e54b179efdc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETLWorkerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETLWorkerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
